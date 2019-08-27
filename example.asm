0d516f75cad1503357fb3e203702683f83dfba7f
Ben Lorenz <bnlrnz@gmail.com>
2019-08-27 15:33:41 +0200

    adds comments to example

--- example.asm
+++ example.asm
@@ -1,6 +1,8 @@
+// This is a comment
 :START 				// Labels start with a ':' and are limited to 31 chars
 LDV 0xFF1			// Load value from defined storage (last line)
-NOT
+NOT 
+# this is also a comment
 STV 0x00
-JMP START 			// Jump to label, the compiler will set the address
+JMP START       	// Jump to label, the compiler will set the address
 0xFF1 0x0FFFFFFF	// Define storage by just providing an address and the value
