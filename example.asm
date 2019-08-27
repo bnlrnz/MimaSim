ad3bfefbc13b1db839e6fad369e99be4a8c10e08
Ben Lorenz <bnlrnz@gmail.com>
2019-08-27 12:57:22 +0200

    renaming/removing example file

--- example.asm
+++ example.asm
@@ -1,6 +1,6 @@
-:START
-LDV 0xFF1
-NOT 
+:START 				// Labels start with a ':' and are limited to 31 chars
+LDV 0xFF1			// Load value from defined storage (last line)
+NOT
 STV 0x00
-JMP START
-0xFF1 0x0FFFFFFF
+JMP START 			// Jump to label, the compiler will set the address
+0xFF1 0x0FFFFFFF	// Define storage by just providing an address and the value
