cc11cc13c49daba6e8c76cc22d79741c19f6a15d
Ben Lorenz <bnlrnz@gmail.com>
2019-08-28 13:46:10 +0200

    adds another example

---
+++ allinstr.asm
@@ -0,0 +1,15 @@
+ADD 0xFF0 // DEBUG: does not make any sense, but includes all instructions
+AND 0xFF0
+OR  0xFF0
+XOR 0xFF0
+LDV 0xFF0
+STV 0xFF0
+LDC 0
+JMP 0x8 
+JMN 0x9
+EQL 0xFF0
+NOT 
+RAR
+RRN 0
+HLT 
+0xFF0 0
